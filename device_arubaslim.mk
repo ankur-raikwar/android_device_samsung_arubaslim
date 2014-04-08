7d1a348b621084834764bb35979900ea03f632a1
ankur850 <ankurraikwar911@gmail.com>
2014-04-08 13:12:23 +0530

    derp

--- device_arubaslim.mk
+++ device_arubaslim.mk
@@ -70,9 +70,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-# Torch
-PRODUCT_PACKAGES += OmniTorch
-
 ## FM
 PRODUCT_COPY_FILES += \
     device/samsung/arubaslim/prebuilt/etc/init.qcom.fm.sh:/system/etc/init.qcom.fm.sh
